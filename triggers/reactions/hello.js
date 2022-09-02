270af8cc792a9d09080abb5101d4ed50f2cf92d8
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2022-09-02 23:51:14 +0200

    Little easter egg lmao

--- triggers/reactions/hello.js
+++ triggers/reactions/hello.js
@@ -11,13 +11,13 @@
  * @type {import('../../typings').TriggerCommand}
  */
 module.exports = {
-	name: ["your", "trigger", "words", "in", "array"],
+	name: ["aerial", "arial", "areal"],
 
 	execute(message, args) {
 		// Put all your trigger code over here. This code will be executed when any of the element in the "name" array is found in the message content.
 
 		message.channel.send({
-			content: "Set this trigger response from `./triggers/reactions/hello.js`",
+			content: "<@310082845387259905> trainiert die fleißig! 🐣",
 		});
 	},
 };
