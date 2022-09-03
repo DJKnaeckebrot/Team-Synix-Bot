a00f40b105a36133e77d08b5050f80f9be7ed532
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2022-09-03 13:38:13 +0200

    Added aerial reaction

--- triggers/reactions/hello.js
+++ triggers/reactions/hello.js
@@ -13,6 +13,7 @@
 module.exports = {
 	name: ["aerial", "arial", "areal"],
 
+	// eslint-disable-next-line no-unused-vars
 	execute(message, args) {
 		// Put all your trigger code over here. This code will be executed when any of the element in the "name" array is found in the message content.
 
