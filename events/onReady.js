8e9aa159b2f9d20aae790b04a0b3b27e0419a097
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2022-11-12 21:10:42 +0100

    added version to status

--- events/onReady.js
+++ events/onReady.js
@@ -15,6 +15,6 @@ module.exports = {
 	 */
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setPresence({ activities: [{ name: '/help | teamsynix.com' }], status: 'online' });
+		client.user.setPresence({ activities: [{ name: '/help | teamsynix.com v1.0.4' }], status: 'online' });
 	},
 };
