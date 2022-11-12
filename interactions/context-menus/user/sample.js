d94eda9a38e6977e35d167a3b7745cb11de189a9
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2022-11-12 15:09:48 +0100

    added move feature to context menu

--- interactions/context-menus/user/sample.js
+++ interactions/context-menus/user/sample.js
@@ -8,16 +8,24 @@
 /**
  * @type {import('../../../typings').ContextInteractionCommand}
  */
+
+const { PermissionsBitField } = require("discord.js");
+
 module.exports = {
 	data: {
-		name: "sample",
+		name: "moveMember",
 		type: 2, // 2 is for user context menus
 	},
 
 	async execute(interaction) {
-		await interaction.reply({
-			content: "I am a sample user context menu.",
-		});
+
+		if (!interaction.member.permissions.has(PermissionsBitField.Flags.MoveMembers)) return interaction.reply({ content: 'You do not have permissions to use this command!', ephemeral: false });
+
+		const member = interaction.targetUser;
+
+		member.voice.setChannel(interaction.member.voice.channel);
+
+		await interaction.reply({ content: 'User has been moved!', ephemeral: true });
 		return;
 	},
 };
