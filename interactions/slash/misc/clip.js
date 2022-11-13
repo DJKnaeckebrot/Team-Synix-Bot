0a84adedd1064445a8245874ab9249d783cc33d8
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2022-11-13 16:09:21 +0100

    fix missing permission

--- interactions/slash/misc/clip.js
+++ interactions/slash/misc/clip.js
@@ -62,7 +62,7 @@ module.exports = {
 
 			await channel.bulkDelete('100', true);
 
-			//channel.permissionOverwrites.edit('784774303714508820', { SendMessages: true });
+			channel.permissionOverwrites.edit('784774303714508820', { SendMessages: true });
 			channel.send("**Liebe** <@&784774303714508820>,\n wir präsentieren euch nun unseren Channel für den **Clip der Woche**. \n Grundsätzlich bietet euch der Channel die Möglichkeit wöchentlich einen von euch erspielten Clip mit der Community zu teilen. \n \n Egal ob ein grandioses Tor, atemberaubender Pass oder eine Parade, die ihresgleichen sucht, zeigt sie der Community um etwas kleines zu gewinnen. \n \n **Hier einmal die Regeln für die Teilnahme:** \n \n - Es werden nur Clips gestattet, die mit \"**Gif Your Game**\" aufgenommen wurden \n - **Jeder** darf nur maximal zwei Clips pro Woche einsenden \n - Es muss sich dabei um eine Szene aus dem Spiel heraus handeln (**kein Bot-Game, kein Freeplay-Training**) \n - Man darf seinen eigenen Clip nicht voten \n \n **Einsendungen beginnen immer Montags.** \n Freitags wird dann die Einsendung von Clips **blockiert** und das **Voting** beginnt. \n Das Voting wird dann am **Sonntag** um **23:59 Uhr** beendet und der Sieger wird angekündigt. \n Der Gewinner wird mit Clip auf unserem Twitter Kanal veröffentlicht. \n Außerdem erhält er **30.000 Credits** für unseren eigenen Gamble Channel <#928408663029342299>. \n Wir würden uns sehr über eure Teilnahme \n freuen und wünschen euch allen viel Glück! \n - <@&1004207195866546376>")
 			interaction.reply({ content: 'Clip channel opened!', ephemeral: true });			
 		}
