fc8ae466693b80290050d5ed1a34ff3b3776fb24
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2022-11-14 17:08:29 +0100

    Add ping

---
+++ interactions/slash/misc/ping.js
@@ -0,0 +1,30 @@
+/**
+ * @file Sample help command with slash command.
+ * @author Naman Vrati & Thomas Fournier
+ * @since 3.0.0
+ * @version 3.3.0
+ */
+
+// Deconstructed the constants we need in this file.
+
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+
+/**
+ * @type {import('../../../typings').SlashInteractionCommand}
+ */
+module.exports = {
+	// The data needed to register slash commands to Discord.
+
+	data: new SlashCommandBuilder()
+		.setName("ping")
+		.setDescription(
+			"Checks if the bot is up"
+		),
+
+	async execute(interaction) {
+
+		interaction.reply(`Websocket heartbeat: ${client.ws.ping}ms.`);
+
+
+	},
+};
