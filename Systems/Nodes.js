85fe8c099cec76e808a611781d4597362155d48d
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2023-01-14 23:37:25 +0100

    Added Systems and utils

---
+++ Systems/Nodes.js
@@ -0,0 +1,14 @@
+const nodes = [
+    {
+        name: "Lavalink",
+        id: "main",
+        host: "0.0.0.0",
+        port: 8080,
+        password: "Massai.123",
+        retryAmount: 15,
+        retryDelay: 6000,
+        secure: false
+    }
+]
+
+module.exports = nodes
